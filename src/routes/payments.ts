06c9a7995b1c686a2cb7a5c8556f465db39f6e53
ntenis5 <nteniskotsiou@gmail.com>
2025-10-04 19:40:02 +0200

    Update payments.ts

--- src/routes/payments.ts
+++ src/routes/payments.ts
@@ -4,7 +4,7 @@ import { supabase } from '../services/supabaseClient';
 
 const router = express.Router();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16'
+  apiVersion: '2025-09-30.clover'
 });
 
 // Create payment intent
