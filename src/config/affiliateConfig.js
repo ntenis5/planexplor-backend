6e7c90b863ff44b4f0c9455c4ff5b06d8207878c
ntenis5 <nteniskotsiou@gmail.com>
2025-10-10 18:37:40 +0200

    Update affiliateConfig.js

--- src/config/affiliateConfig.js
+++ src/config/affiliateConfig.js
@@ -1,16 +1,14 @@
 // src/config/affiliateConfig.js
-export const getAffiliateConfig = () => ({
+export const affiliateConfig = {
   booking: {
-    baseUrl: process.env.BOOKING_BASE_URL || 'https://distribution-xml.booking.com/2.0',
-    apiKey: process.env.BOOKING_COM_API_KEY,
-    endpoints: {
-      hotels: '/json/hotels',
-      availability: '/json/hotelAvailability'
-    }
+    baseUrl: process.env.BOOKING_BASE_URL,
+    apiKey: process.env.BOOKING_COM_API_KEY, // Merr nga environment
+    secret: process.env.BOOKING_API_SECRET
   },
   tripadvisor: {
-    baseUrl: process.env.TRIPADVISOR_BASE_URL || 'https://api.tripadvisor.com/api',
-    apiKey: process.env.TRIPADVISOR_API_KEY,
-    version: '1.0'
+    baseUrl: process.env.TRIPADVISOR_BASE_URL,
+    apiKey: process.env.TRIPADVISOR_API_KEY
   }
-});
+};
+
+// NUK ruaj keys direkt në kod!
