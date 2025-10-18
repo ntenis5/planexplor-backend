f96ade61768958a29cfc824191f62b62e0340baa
ntenis5 <nteniskotsiou@gmail.com>
2025-10-18 11:28:37 +0200

    Update start.sh

--- . railway/start.sh
+++ . railway/start.sh
@@ -4,9 +4,4 @@ set -e
 export NODE_ENV=production
 export PORT=$PORT
 
-if ! command -v ts-node &> /dev/null; then
-    echo "Installing ts-node..."
-    npm install -g ts-node
-fi
-
-exec npx ts-node src/app.ts
+exec npx ts-node --esm src/app.ts
