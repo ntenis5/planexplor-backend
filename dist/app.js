0523a379383fdd5c324131d2ab50b137ddb30db4
ntenis5 <nteniskotsiou@gmail.com>
2025-10-18 11:19:24 +0200

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.json({ message: 'Planexplor API is running!' });
